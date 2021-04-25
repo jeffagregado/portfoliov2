ff6367e2fef2b124278d5ba37cddf9b325116861
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-04-26 00:36:11 +0800

    next.config:

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = (phase, { defaultConfig }) => {
   if ('sassOptions' in defaultConfig) {
     defaultConfig['sassOptions'] = {
       //includePaths: ['./styles/'],
       includePaths: [path.join(__dirname, 'styles')],
-      prependData: `@use 'utilities' as *;`,
+      prependData: `@use './styles/utilities/_index.scss' as *;`,
     }
   }
   return defaultConfig

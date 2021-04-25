57c70daaa52c494554a15c322489d20391f77b7f
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-04-26 01:56:55 +0800

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-/* const path = require('path')
+const path = require('path')
 
 module.exports = (phase, { defaultConfig }) => {
   if ('sassOptions' in defaultConfig) {
     defaultConfig['sassOptions'] = {
       //includePaths: ['./styles/'],
       includePaths: [path.join(__dirname, 'styles')],
-      prependData: `@import './styles/utilities/index';`,
+      prependData: `@use 'Utilities' as *;`,
     }
   }
   return defaultConfig
-} */
+}
 
 /* module.exports = (phase, { defaultConfig }) => {
     if ('sassOptions' in defaultConfig) {
